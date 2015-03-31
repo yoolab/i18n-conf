3223d0c645f7f90a4b2454de14b28842dad48d91
Martino Piccinato <mp@yoolab.com>
2015-03-31 22:29:38 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,13 @@
+# i18n conf 
+
+i18n configuration package for [meteorjs](https://www.meteor.com/). This package just provide basic support
+for i18 configuration, it is meant to be used bu other i18n package in order to achieve common tasks like
+configuring supported languages, autoconfigure language etc.
+
+# Work in progress
+
+See [this issue](https://github.com/yoolab/iron-router-i18n/issues/46) for the reason I'm creating this package.
+
+## License
+
+MIT
---
+++ package.js
@@ -0,0 +1,37 @@
+Npm.depends({locale: "0.0.20"});
+
+Package.describe({
+    summary: 'i18n configuration package',
+    git: "https://github.com/yoolab/i18n-conf.git",
+    name: "martino:i18n-conf",
+    version: '0.1.0'
+});
+
+Package.onUse(function (api) {
+
+    api.versionsFrom('1.0');
+
+    api.use('tracker@1.0.5', ['client', 'server']);
+    api.use('underscore@1.0.2', ['client', 'server']);
+
+    api.addFiles('lib/i18nconf.js', ['client', 'server']);
+    api.addFiles('lib/i18nconf_client.js', ['client']);
+    api.addFiles('lib/i18nconf_server.js', ['server']);
+    api.addFiles('lib/i18nconf_init.js', ['client', 'server']);
+
+    api.export('I18NConf', ['client', 'server']);
+
+});
+
+Package.onTest(function (api) {
+
+    api.use('underscore@1.0.2', ['client', 'server']);
+    api.use('i18n-conf@0.1.0', ['client', 'server']);
+
+    api.use('tinytest', ['client', 'server']);
+    api.use('test-helpers', ['client', 'server']);
+
+    api.addFiles('test/i18nconf_test.js', ['client', 'server']);
+
+
+});
